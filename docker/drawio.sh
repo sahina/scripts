4ea4ac7de7b804ce8c60d3b726894f2840a10b30
altug <altugsahin@gmail.com>
2020-05-14 09:59:07 -0400

    draw io

---
+++ docker/drawio.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# create volume
+docker volume create --driver local --name=drawio
+
+docker run -it --rm --name=drawio -p 8080:8080 -p 8443:8443 jgraph/draw.io
+
+# bring up the container
+docker run --name=drawio \
+    -p 8080:8080 \
+    -p 8443:8443 \
+    --detach \
+    --rm \
+fjudith/draw.io:latest
