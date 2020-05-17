f07b2c98267dff2b02e3c4e1389547521b8ce49f
altug <altugsahin@gmail.com>
2020-05-16 20:40:43 -0400

    draw io fixed

--- docker/drawio.sh
+++ docker/drawio.sh
@@ -3,8 +3,6 @@
 # create volume
 docker volume create --driver local --name=drawio
 
-docker run -it --rm --name=drawio -p 8080:8080 -p 8443:8443 jgraph/draw.io
-
 # bring up the container
 docker run --name=drawio \
     -p 8080:8080 \
