7b58f8824f7cb65deba60b3e21b68ea4acbc5df6
altug <altugsahin@gmail.com>
2020-10-14 06:26:43 -0400

    typo

--- docker/portainer.sh
+++ docker/portainer.sh
@@ -2,7 +2,6 @@
 
 docker volume create portainer_data
 
-<<<<<<< HEAD
 docker run -d -p 8000:8000 \
 	-p 9000:9000 \
 	--name=portainer \
@@ -10,17 +9,4 @@ docker run -d -p 8000:8000 \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v portainer_data:/data \
 portainer/portainer-ce
-=======
-docker run -d \
-    -p 8000:8000 \
-    -p 9000:9000 \
-    --name=portainer \
-    --restart=always \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v portainer_data:/data \
-<<<<<<< HEAD
-portainer/portainer-ce
-=======
-portainer/portainer
->>>>>>> f07b2c98267dff2b02e3c4e1389547521b8ce49f
->>>>>>> 381fb1fc04be950bf4d1c136b80f7e4edb916bbb
+
