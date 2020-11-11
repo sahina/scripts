a91c8843fb712505407f4b1756597800dc80195d
altug <altugsahin@gmail.com>
2020-11-10 21:13:24 -0500

    axon added

---
+++ docker/axon.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+docker volume create axon_data
+docker volume create axon_events
+docker volume create axon_config
+
+docker run -d --rm --name axonserver \
+    -p 8024:8024 \
+    -p 8124:8124 \
+    -v axon_data:/data \
+    -v axon_events:/eventdata \
+    -v axon_config:/config \
+axoniq/axonserver
